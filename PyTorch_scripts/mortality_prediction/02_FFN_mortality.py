e56ff27af6a82c0e295bcbc495b35e8b93f1f5f8
ChristinaParrott <chipps73@gmail.com>
2023-04-09 11:04:10 -0400

    refactoring

--- PyTorch_scripts/mortality_prediction/02_FFN_mortality.py
+++ PyTorch_scripts/mortality_prediction/02_FFN_mortality.py
@@ -11,7 +11,6 @@
 import pickle
 import matplotlib.pyplot as plt
 import random
-import argparse
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -22,26 +21,34 @@ from sklearn.metrics import roc_auc_score as roc
 import gc
 
 RANDOM_SEED = 6250
+INPUT_DATA = 'data/prepared_data.npz'
+INPUT_DATA2 = 'data/prepared_data_deathTime.npz'
+VERBOSE = True
+LOG_INTERVAL = 2 # The freqeuncy to output loss data to the console
+
+# HYPERPARAMETERS
+HIDDEN_DIM_SIZE = 50
+BATCH_SIZE = 50
+N_EPOCHS = 50
+LEARNING_RATE = 0.001
+DROP_OUT = 0.5
+K_FOLDS = 5
+WITH_CCS = False
 
 class Network(nn.Module):
-    def __init__(self):
+    def __init__(self, input_dim):
         super().__init__()
-
-        ARGS.inputdim = ARGS.numberOfInputCUIInts + ARGS.numberOfInputCCSInts if ARGS.withCCS else ARGS.numberOfInputCUIInts
-        self.fc1 = nn.Linear(ARGS.inputdim, ARGS.hiddenDimSize)
-        self.fc2 = nn.Linear(ARGS.hiddenDimSize, 3)
+        self.fc1 = nn.Linear(input_dim, HIDDEN_DIM_SIZE)
+        self.fc2 = nn.Linear(HIDDEN_DIM_SIZE, 3)
         self.relu = nn.ReLU()
-        self.sigmo = nn.Sigmoid()
-        self.dropout = nn.Dropout(p=0.5)
+        self.dropout = nn.Dropout(p=DROP_OUT)
 
     def forward(self, x):
-        # Prop input through FFN
         x = self.fc1(x)
         x = self.relu(x)
         x = self.dropout(x)
         x = self.fc2(x)
         return x
-
 
 class my_dataset(dt.Dataset):
     def __init__(self, data, label):
@@ -56,185 +63,148 @@ class my_dataset(dt.Dataset):
 
 
 def load_tensors():
-    # -----------------------------------------
-    # pickle input map - each entry is a pair (subject_id, [(hadm_id,admittime, [CUIsvector], [CCSsvector])]
-    # notesVectors_trainMapX = pickle.load(open(ARGS.inputFileNotes, 'rb'))
-    subjecttoadm_map = pickle.load(open(ARGS.inputdata, 'rb'))
-    subjecttodeath_map = pickle.load(open(ARGS.inputdata2, 'rb'))
-    setOfDistinctCUIs = set()
-    setOfDistinctCCSs = set()
-    cuitoint = dict()
-    ccstoint = dict()
-    for subject in subjecttoadm_map.keys():
-        patientData = subjecttoadm_map[subject]
-        for ithAdmis in patientData:
-            for CUIcode in ithAdmis[2]:
-                setOfDistinctCUIs.add(CUIcode)
-            for CCScode in ithAdmis[3]:
-                setOfDistinctCCSs.add(CCScode)
-    for i, cui in enumerate(setOfDistinctCUIs):
-        cuitoint[cui] = i
-    for i, ccs in enumerate(setOfDistinctCCSs):
-        ccstoint[ccs] = i
-    print("-> " + str(
-        len(subjecttoadm_map)) + " patients' CUI notes and CCS codes at dimension 0 for file: " + ARGS.inputdata)
-    ARGS.numberOfInputCUIInts = len(setOfDistinctCUIs)
-    ARGS.numberOfInputCCSInts = len(setOfDistinctCCSs)
-    # -------------------------------------------
-    ARGS.numberOfOutputCodes = 1
-    print('Remaining patients:', len(subjecttoadm_map))
-    # Convert everything to list of list of list (patient x admission x CUInote_vector/mortality
-    # to ease the manipulation in batches
-    vectors_trainListX = []
-    mortality_trainListY = []
-    maxSeqLength = 0
-    for pID, adList in subjecttoadm_map.items():
-        for i, adm in enumerate(adList):
-            one_hot_CUI = [0] * ARGS.numberOfInputCUIInts
-            one_hot_CCS = [0] * ARGS.numberOfInputCCSInts
+    subject_adm_map = pickle.load(open(INPUT_DATA, 'rb'))
+    subject_death_map = pickle.load(open(INPUT_DATA2, 'rb'))
+
+    cui_set, ccs_set = set(), set()
+    cui_to_int, ccs_to_int = {}, {}
+
+    for subject in subject_adm_map.keys():
+        patient_data = subject_adm_map[subject]
+        for ith_adm in patient_data:
+            for cui_code in ith_adm[2]:
+                cui_set.add(cui_code)
+            for ccs_code in ith_adm[3]:
+                ccs_set.add(ccs_code)
+    for i, cui in enumerate(cui_set):
+        cui_to_int[cui] = i
+    for i, ccs in enumerate(ccs_set):
+        ccs_to_int[ccs] = i
+
+    if VERBOSE:
+        print(f"{len(subject_adm_map)} patients CUI notes and CCS codes at dimension 0 for file: {INPUT_DATA}")
+
+    num_cui_ints = len(cui_set)
+    num_ccs_ints = len(ccs_set)
+
+    if VERBOSE:
+        print(f'Remaining patients: {len(subject_adm_map)}')
+
+    vectors_train_x, mortality_train_y = [], []
+
+    for patient_id, adm_list in subject_adm_map.items():
+        for i, adm in enumerate(adm_list):
+            one_hot_cui = [0] * num_cui_ints
+            one_hot_ccs = [0] * num_ccs_ints
+
             for cui_int in adm[2]:
-                one_hot_CUI[cuitoint[cui_int]] = 1
+                one_hot_cui[cui_to_int[cui_int]] = 1
             for ccs_int in adm[3]:
-                one_hot_CCS[ccstoint[ccs_int]] = 1
-            one_hot_X = one_hot_CUI + one_hot_CCS if ARGS.withCCS else one_hot_CUI
-            vectors_trainListX.append(one_hot_X)
-            # compute patient mortality
-            Y_sample = [0] * 3
-            if (subjecttodeath_map[pID] - adm[4]).days <= 0:
-                Y_sample[0] = 1
-            elif (subjecttodeath_map[pID] - adm[4]).days <= 30:
-                Y_sample[1] = 1
+                one_hot_ccs[ccs_to_int[ccs_int]] = 1
+
+            one_hot_x = one_hot_cui + one_hot_ccs if WITH_CCS else one_hot_cui
+            vectors_train_x.append(one_hot_x)
+
+            y_sample = [0] * 3
+            if (subject_death_map[patient_id] - adm[4]).days <= 0:
+                y_sample[0] = 1
+            elif (subject_death_map[patient_id] - adm[4]).days <= 30:
+                y_sample[1] = 1
             else:
-                Y_sample[2] = 1
-            mortality_trainListY.append(Y_sample)
+                y_sample[2] = 1
+            mortality_train_y.append(y_sample)
 
-    # Randomize in dimension 0 (patients order) keeping the notes and mortality in sync
-    mapIndexPosition = list(zip(vectors_trainListX, mortality_trainListY))
-    random.shuffle(mapIndexPosition)
-    vectors_trainListX, mortality_trainListY = zip(*mapIndexPosition)
+    map_index_position = list(zip(vectors_train_x, mortality_train_y))
+    random.shuffle(map_index_position)
+    vectors_train_x, mortality_train_y = zip(*map_index_position)
 
-    return vectors_trainListX, mortality_trainListY
-
+    input_dim = num_ccs_ints + num_cui_ints if WITH_CCS else num_cui_ints
+    return vectors_train_x, mortality_train_y, input_dim
 
 def split(a, n):
     k, m = divmod(len(a), n)
     return (a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
 
-
 def train():
-    data_x, data_y = load_tensors()
-    #
-    # use_cuda = torch.cuda.is_available()
-    # print("Available GPU :", use_cuda)
-    # if use_cuda:
-    #     torch.cuda.set_device(1)
+    data_x, data_y, input_dim = load_tensors()
 
-    sizedata = len(data_x)
-    print("Data of size:", sizedata)
-    k = ARGS.kFold
-    # Split dataset into 5 sub-datasets
-    splitted_x = list(split(data_x, 5))
-    splitted_y = list(split(data_y, 5))
+    if VERBOSE:
+        print(f"Data of size: {len(data_x)}")
+
+    x_split = list(split(data_x, 5))
+    y_split = list(split(data_y, 5))
 
     del data_x
     del data_y
     gc.collect()
-    print("Memory freed")
 
-    AUC_folds = []
-    for ind_i in range(0, k):
-        # Prepare X_train Y_train X_test Y_test
-        X_test = splitted_x[ind_i]
-        Y_test = splitted_y[ind_i]
-        # Deep copy, otherwise iteration problem
-        copysplitX = list(splitted_x)
-        copysplitY = list(splitted_y)
-        del copysplitX[ind_i]
-        del copysplitY[ind_i]
-        X_train = copysplitX
-        Y_train = copysplitY
-        model = Network() #.cuda()
+    auc_folds = []
+    for ind_i in range(0, K_FOLDS):
+        X_test = x_split[ind_i]
+        Y_test = y_split[ind_i]
 
-        # with torch.cuda.device(1):
-        # Hyperparameters :
-        n_epochs = ARGS.nEpochs
-        batchsize = ARGS.batchSize
-        learning_rate = ARGS.lr
-        log_interval = 2
+        split_x_cpy = list(x_split)
+        split_y_cpy = list(y_split)
+
+        del split_x_cpy[ind_i]
+        del split_y_cpy[ind_i]
+
+        x_train = split_x_cpy
+        y_train = split_y_cpy
+
+        model = Network(input_dim)
         criterion = nn.BCEWithLogitsLoss()
-        optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-        # optimizer = optim.SGD(model.parameters(), lr=learning_rate)
-        # Data loader0
-        tensor_x = torch.ByteTensor(
-            np.concatenate([np.array(plist, dtype=np.uint8) for plist in X_train]).tolist()) #.cuda()
-        tensor_y = torch.ByteTensor(
-            np.concatenate([np.array(plist, dtype=np.uint8) for plist in Y_train]).tolist()) #.cuda()
-        print("Shape X:", tensor_x.size(), "Shape Y:", tensor_y.size())
-        dataset = dt.TensorDataset(tensor_x, tensor_y)  # create your dataset
-        print("TRAINLOADER")
+        optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+        tensor_x = torch.ByteTensor(np.concatenate([np.array(plist, dtype=np.uint8) for plist in x_train]).tolist())
+        tensor_y = torch.ByteTensor(np.concatenate([np.array(plist, dtype=np.uint8) for plist in y_train]).tolist())
+
+        if VERBOSE:
+            print(f"Shape X: {tensor_x.size()} Shape Y: {tensor_y.size()}")
+
+        dataset = dt.TensorDataset(tensor_x, tensor_y)
         train_loader = dt.DataLoader(
             dataset,
-            batch_size=batchsize,
+            batch_size=BATCH_SIZE,
             shuffle=True)
+
         gc.collect()
 
         losses = []
-
-        # Training
-        for epoch in range(n_epochs):
+        for epoch in range(N_EPOCHS):
             for batch_idx, (data, target) in enumerate(train_loader):
-                # data, target = data.cuda(), target.cuda()
                 data, target = Variable(data.float()), Variable(target.float())
                 optimizer.zero_grad()
                 net_out = model(data)
                 loss = criterion(net_out, target)
                 loss.backward()
                 optimizer.step()
-                if batch_idx % log_interval == 0:
-                    print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: '.format(
-                        epoch, batch_idx * len(data), len(train_loader.dataset),
-                               100. * batch_idx / len(train_loader)))
-                    print(loss.data)
-                    losses.append(loss.item())
+                if batch_idx % LOG_INTERVAL == 0:
+                    if VERBOSE:
+                        print(f'Train Epoch: {epoch} [{batch_idx * len(data)}/{len(train_loader.dataset)} ({100. * batch_idx/len(train_loader)}%)]')
+                        print(f'Loss: {loss.data}')
+            losses.append(loss.item())
 
-        print("Training done - deleting variables and starting test")
-        dataset = None
-        train_loader = None
-        tensor_x = None
-        tensor_y = None
-        arrX = None
-        arrY = None
-        numplist = None
-        copysplitX = None
-        copysplitY = None
         del dataset
         del train_loader
         del tensor_x
         del tensor_y
-        del arrX
-        del arrY
-        del numplist
-        del copysplitX
-        del copysplitY
+        del split_x_cpy
+        del split_y_cpy
         gc.collect()
 
-        # Test loader
-        tensor_x = torch.Tensor(np.array(X_test).tolist()) #.cuda()  # transform to torch tensor
-        tensor_y = torch.Tensor(np.array(Y_test).tolist()) #.cuda()
-
-        dataset = dt.TensorDataset(tensor_x, tensor_y)  # create your dataset
+        tensor_x = torch.Tensor(np.array(X_test).tolist())
+        tensor_y = torch.Tensor(np.array(Y_test).tolist())
+        dataset = dt.TensorDataset(tensor_x, tensor_y)
         test_loader = dt.DataLoader(
             dataset,
-            batch_size=batchsize,
+            batch_size=BATCH_SIZE,
             shuffle=False)
 
-        # Testing
         model.eval()
 
-        # AUROC
-        AUC_list = list()
-        YTRUE = None
-        YPROBA = None
+        y_true = None
+        y_prob = None
         for (data, targets) in test_loader:
             x, labels = Variable(data.float()), Variable(targets.float())
             outputs = model(x)
@@ -242,50 +212,32 @@ def train():
             x = x.detach().cpu().numpy()
             labels = labels.detach().cpu().numpy()
             for batch_x, batch_true, batch_prob in zip(x, labels, outputs):
-                YTRUE = np.concatenate((YTRUE, [batch_true]), axis=0) if YTRUE is not None else [batch_true]
-                YPROBA = np.concatenate((YPROBA, [batch_prob]), axis=0) if YPROBA is not None else [batch_prob]
+                y_true = np.concatenate((y_true, [batch_true]), axis=0) if y_true is not None else [batch_true]
+                y_prob = np.concatenate((y_prob, [batch_prob]), axis=0) if y_prob is not None else [batch_prob]
+
         try:
-            ROC_avg_score = roc(YTRUE, YPROBA, average='macro', multi_class='ovo')
-            print("ROC Average Score:", ROC_avg_score)
-            AUC_folds.append(ROC_avg_score)
+            roc_avg_score = roc(y_true, y_prob, average='macro', multi_class='ovo')
+            if VERBOSE:
+                print(f"ROC Average Score:{roc_avg_score}")
+            auc_folds.append(roc_avg_score)
+
         except:
-            print("ROC was undefined because classes are all the same")
+            if VERBOSE:
+                print("ROC was undefined because classes are all the same")
 
-    epochs = [n + 1 for n in range(n_epochs)]
-    plot_loss(epochs, losses)
+    plot_loss(losses)
 
-    # Output score of each fold + average
-    print("AUC_scores: ", AUC_folds)
-    print("Mean: ", sum(AUC_folds) / len(AUC_folds))
+    print(f"AUC_scores: {auc_folds}")
+    print(f"Mean: {sum(auc_folds) / len(auc_folds)}")
 
-
-def plot_loss(epochs, train_loss):
-    plt.plot(epochs, train_loss, label='Training Loss')
+def plot_loss(train_loss):
+    plt.plot(train_loss, label='Training Loss')
     plt.title("Training Loss")
     plt.xlabel("Epochs")
     plt.ylabel("Loss")
     plt.savefig("training_loss.png")
 
-
-def parse_arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--inputdata', type=str, default='data/prepared_data.npz', metavar='<visit_file>')
-    parser.add_argument('--inputdata2', type=str, default='data/prepared_data_deathTime.npz', metavar='<visit_file>')
-    parser.add_argument('--hiddenDimSize', type=int, default=50, help='Size of FFN hidden layer')
-    parser.add_argument('--batchSize', type=int, default=50, help='Batch size.')
-    parser.add_argument('--nEpochs', type=int, default=50, help='Number of training iterations.')
-    parser.add_argument('--lr', type=float, default=0.001, help='Learning rate.')
-    parser.add_argument('--dropOut', type=float, default=0.5, help='FFN Dropout.')
-    parser.add_argument('--kFold', type=int, default=5, help='K value (int) of K-fold cross-validation.')
-    parser.add_argument('--withCCS', type=int, default=0, help="add CCS features in the input.")
-
-    ARGStemp = parser.parse_args()
-    return ARGStemp
-
-
 if __name__ == '__main__':
-    global ARGS
-    ARGS = parse_arguments()
     torch.manual_seed(RANDOM_SEED)
     random.seed(RANDOM_SEED)
     train()
